13af3aa75ef20adc58c84a98729ea418ec1fab91
Kieran Collins <kiecol@protonmail.com>
2022-11-16 03:44:16 +0000

    Add code to calculate two random numbers for the game questiion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
-        button.addEventListener("click", function() {
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked submit!");
             } else {
@@ -13,8 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
 
+    /** Creates two random numbers between 1 & 25 
+     * (It creates a demical number between 0 & 1, then multiplies it by 25, 
+     * [max result is now 24], then it adds a 1 so that 0 is never generated) */
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -22,25 +31,25 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
-    
+
 }
 
 function incrementScore() {
-    
+
 }
 
 function incrementWrongAnswer() {
-    
+
 }
 
 function displayAdditionQuestion() {
-    
+
 }
 
 function displaySubtractQuestion() {
-    
+
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
